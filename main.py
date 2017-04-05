d1e991a400aa391d173a91aaade862f1ef7d1ce9
euaru <bintang.nafsul@gmail.com>
2017-04-05 20:01:28 +0700

    Memperbaiki pembuatan dan akses database
    Menambahkan modul create_engine untuk membuat database, memindahkan
    class table database ke dalam file main.py

--- main.py
+++ main.py
@@ -3,23 +3,70 @@ from flask_bootstrap import Bootstrap
 from models import Pemain
 from forms import RegistrationForm
 from models import db
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+engine = create_engine('sqlit:///quizdatabase.db', echo=True)
+Base = declarative_base()
+
+class Pemain(Base):
+	id = db.Column(db.Integer, primary_key=True)
+	usia = db.Column(db.Integer)
+	jeniskelamin = db.Column(db.String(10))
+	area = db.Column(db.String(20))
+	main = db.relationship('SoalDiJawab', backref='user', lazy='dynamic')
+	def __init__(self, usia, jeniskelamin, area):
+		self.usia = usia
+		self.jeniskelamin = jeniskelamin
+		self.area = area
+	def __repr__(self):
+		return '<Area %r>' % self.area
+
+class SoalDiJawab(Base):
+	id = Column(Integer, primary_key=True)
+	idpemain = Column(Integer, ForeignKey('pemain.id'))
+	idsoal = Column(Integer, ForeignKey('soal.id'))
+	jawabanpemain = Column(String(50))
+	waktujawab = Column(Time)
+	def __init__(self, idpemain, idsoal, jawabanpemain, waktujawab):
+		self.idpemain = idpemain
+		self.idsoal = idsoal
+		self.jawabanpemain = jawabanpemain
+		self.waktujawab = waktujawab
+	def __repr__(self):
+		return '<Jawaban: %r>' % self.jawabanpemain
+
+
+class Soal(Base):
+	id = dColumn(Integer, primary_key=True)
+	pertanyaan = Column(String(1000))
+	jawaban = Column(String(50))
+	soal_di_jawab = relationship('SoalDiJawab', backref='jawabansoal', lazy='dynamic')
+	def __init__(self, pertanyaan, jawaban):
+		self.pertanyaan = pertanyaan
+		self.jawaban = jawaban
+	def __repr__(self):
+		return '<Soal: %r>' % self.pertanyaan
+		
+Base.metadata.create_all(engine)
+
 @app.route('/register', methods=['GET', 'POST'])
 
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-	db.create_all()
+        create_all()
         pemain = Pemain(usia=form.usia.data,jeniskelamin=form.jeniskelamin.data, area=form.area.data)
-	print pemain.usia
-	print pemain
-        db.session.add(pemain)
-        db.session.commit()
+        session.add(pemain)
+        session.commit()
     #    flash('Selamat bermain!')
         #return redirect(url_for(''))
 
